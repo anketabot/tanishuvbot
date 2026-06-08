e2ce9b742693c3e5158582a010a91d52c21ddf87
anketabot <anketa967@gmail.com>
2026-06-08 20:44:29 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "8224380614:AAF7GXGvWg5JD5T2VjYYlRBSQLq_YYI18zw"
 DATABASE_URL = "postgresql://postgres:phUAWonpnpDWaGSQUgeuyDrbqohmsDzi@acela.proxy.rlwy.net:51349/railway"
 WEBAPP_URL = "https://anketabot.github.io/tanishuvweb/"  # GitHub Pages, Netlify yoki boshqa hosting URL
-ADMIN_PASSWORD = "admin123"  # Admin panel uchun oddiy parol. O'zingizga moslab o'zgartiring.
+ADMIN_PASSWORD = "1"  # Admin panel uchun oddiy parol. O'zingizga moslab o'zgartiring.
