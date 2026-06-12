57288139b69fe77414f8c605756d5393fcbd8ed3
anketabot <anketa967@gmail.com>
2026-06-13 02:48:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 BOT_TOKEN = "8224380614:AAF7GXGvWg5JD5T2VjYYlRBSQLq_YYI18zw"
 DATABASE_URL = "postgresql://postgres:phUAWonpnpDWaGSQUgeuyDrbqohmsDzi@acela.proxy.rlwy.net:51349/railway"
-WEBAPP_URL = "https://anketabot.github.io/tanishuvweb/"  # GitHub Pages, Netlify yoki boshqa hosting URL
-ADMIN_PASSWORD = "1"  # Admin panel uchun oddiy parol. O'zingizga moslab o'zgartiring.
+WEBAPP_URL = "https://anketabot.github.io/tanishuvweb/"
+ADMIN_PASSWORD = "1"
+
+# Guruh sozlamalari
+GROUP_CHAT_ID = -1003888172359
+GROUP_INVITE_LINK = "https://t.me/+HA4J8P7lht0zZTdi"
