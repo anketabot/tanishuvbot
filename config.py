bfe73de613dcb60478db32913d99b90836761b47
anketabot <anketa967@gmail.com>
2026-06-24 00:37:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-BOT_TOKEN = "8694682300:AAEyCnECVCsq5Ebjl8rLMkMXY_Dg_UlLgJc"
-DATABASE_URL = "postgresql://postgres:phUAWonpnpDWaGSQUgeuyDrbqohmsDzi@acela.proxy.rlwy.net:51349/railway"
-WEBAPP_URL = "https://anketabot.github.io/tanishuvweb/"
-ADMIN_PASSWORD = "1"
+import os
+from dotenv import load_dotenv
 
-# Guruh sozlamalari
-GROUP_CHAT_ID = -1003888172359
-GROUP_INVITE_LINK = "https://t.me/+HA4J8P7lht0zZTdi"
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+WEBAPP_URL = os.getenv("WEBAPP_URL")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
+GROUP_INVITE_LINK = os.getenv("GROUP_INVITE_LINK")
